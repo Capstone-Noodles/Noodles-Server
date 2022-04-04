113e2f26431087d1d4de60ce6630670714e5e15a
wormjoo <wormjoo@naver.com>
2022-04-04 21:49:59 +0900

    Add: OnePostInfo API

--- src/main/java/capston/noodles/Post/repository/PostRepository.java
+++ src/main/java/capston/noodles/Post/repository/PostRepository.java
@@ -2,6 +2,7 @@ package capston.noodles.Post.repository;
 
 import capston.noodles.Post.mapper.PostMapper;
 import capston.noodles.Post.model.response.AllPostResponse;
+import capston.noodles.Post.model.response.OnePostResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +15,9 @@ public class PostRepository {
 
     public List<AllPostResponse> getAllPostInfo(double longitude, double latitude) {
         return postMapper.getAllPostInfo(longitude, latitude);
+    }
+
+    public List<OnePostResponse> getOnePostInfo(long postIdx) {
+        return postMapper.getOnePostInfo(postIdx);
     }
 }
--- src/main/java/capston/noodles/Post/service/PostService.java
+++ src/main/java/capston/noodles/Post/service/PostService.java
@@ -1,6 +1,7 @@
 package capston.noodles.Post.service;
 
 import capston.noodles.Post.model.response.AllPostResponse;
+import capston.noodles.Post.model.response.OnePostResponse;
 import capston.noodles.Post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,9 @@ public class PostService {
 
     public List<AllPostResponse> getAllPostInfo(double longitude, double latitude) {
         return postRepository.getAllPostInfo(longitude, latitude);
+    }
+
+    public List<OnePostResponse> getOnePostInfo(long postIdx) {
+        return postRepository.getOnePostInfo(postIdx);
     }
 }
