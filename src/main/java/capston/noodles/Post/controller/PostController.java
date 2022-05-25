5f7f28fffe87e481358094edcbbb4254344c2a94
ji-mango <susie000301@naver.com>
2022-05-26 06:15:28 +0900

    Fix

--- src/main/java/capston/noodles/Post/controller/PostController.java
+++ src/main/java/capston/noodles/Post/controller/PostController.java
@@ -44,7 +44,7 @@ public class PostController {
 
     // 게시물 업로드
     @PostMapping("/posts/write")
-    public ResponseMessage uploadPost(@RequestPart(value = "uploadDto") UploadPostDto uploadPostDto, @RequestParam("imageFileList") List<MultipartFile> imageFileList, HttpServletRequest request) throws IOException {
+    public ResponseMessage uploadPost(@RequestPart(value = "uploadDto") UploadPostDto uploadPostDto, @RequestPart("imageFileList") List<MultipartFile> imageFileList, HttpServletRequest request) throws IOException {
         String token = request.getHeader("x-auth-token");
         String userIdxStr = jwtProvider.getUserPk(token);
         long userIdx = Long.parseLong(userIdxStr);
