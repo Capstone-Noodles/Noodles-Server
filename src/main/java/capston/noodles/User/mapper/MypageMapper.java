ba4365830f38fda760a3f164b4543e6e6012699b
Seungyong Choi <ppyooy336@naver.com>
2022-05-29 13:45:31 +0900

    Add updateMyProfile api

--- src/main/java/capston/noodles/User/mapper/MypageMapper.java
+++ src/main/java/capston/noodles/User/mapper/MypageMapper.java
@@ -1,5 +1,6 @@
 package capston.noodles.User.mapper;
 
+import capston.noodles.User.model.entity.dto.UpdateProfileDto;
 import capston.noodles.User.model.response.MypageResponse;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -9,4 +10,6 @@ import java.util.List;
 @Mapper
 public interface MypageMapper {
     List<MypageResponse> getUserInfo(@Param("userId") long userId);
+
+    int updateProfile(UpdateProfileDto dto);
 }
